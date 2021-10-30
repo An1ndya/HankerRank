70f65587dc0803d73d1e3df756c0ddfd7af1e26f
Anindya Biswas <anindya33046@gmail.com>
2021-10-31 02:08:19 +0600

    easy practice

--- certification/SQL/Select By ID.sql
+++ certification/SQL/Select By ID.sql
@@ -1,7 +1,8 @@
+
 /*
     Enter your query here and follow these instructions:
     1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
     2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
     3. Type your code immediately after comment. Don't leave any blank line.
 */
-select * from city c where c.ID = 1661;
+select t.name from city t where t.CountryCode='USA' and t.population>120000;
