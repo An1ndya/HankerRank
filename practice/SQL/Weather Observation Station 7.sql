423205fbc7f3cf53a01544e0314ba7bdf95adfe0
Anindya Biswas <anindya33046@gmail.com>
2021-10-31 02:28:50 +0600

    sql practice

--- practice/SQL/Weather Observation Station 7.sql
+++ practice/SQL/Weather Observation Station 7.sql
@@ -1,8 +1,7 @@
-
 /*
     Enter your query here and follow these instructions:
     1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
     2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
     3. Type your code immediately after comment. Don't leave any blank line.
 */
-select t.name from city t where t.CountryCode='USA' and t.population>120000;
+select distinct t.city from STATION t where  Lcase(right(t.city,1)) in ('a','e','i','o','u') ;
